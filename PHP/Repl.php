c2e4010ba151f3bc194d39744fd4b31db73f8c49
Ian Eure <ian.eure@gmail.com>
2009-03-27 21:15:33 -0700

    Refactor to a class.

--- PHP/Repl.php
+++ PHP/Repl.php
@@ -1,3 +1,4 @@
+#! /usr/bin/php
 <?php
 
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
@@ -14,20 +15,152 @@
  * @filesource
  */
 
-$prompt = "php> ";
-$__stdin__  = fopen('php://stdin', 'r');
-$__code__ = '"PHP_Repl v0.1"';
-do {
-    if (substr($__code__, -1) != ';') {
-        $__code__ .= ';';
-    }
-    if (strpos($__code__, 'return') !== 0) {
-        $__code__ = 'return ' . $__code__;
+/**
+ * PHP_Repl
+ *
+ * @package    PHP_Repl
+ * @author     Ian Eure <ieure@php.net>
+ */
+class PHP_Repl
+{
+    /**
+     * Where we're reading input from
+     *
+     * @var resource
+     */
+    private $input;
+
+    /**
+     * The prompt
+     *
+     * @var string
+     */
+    public $prompt = 'php> ';
+
+
+    /**
+     * Constructor
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->input = fopen('php://stdin', 'r');
     }
 
-    $res = eval($__code__);
-    var_dump($res);
-    echo $prompt;
-} while ($__code__ = trim(fgets($__stdin__)));
+    /**
+     * Destructor
+     *
+     * @return void
+     */
+    public function __destruct()
+    {
+        fclose($this->input);
+    }
+
+    /**
+     * Run the main loop
+     *
+     * @return void
+     */
+    public function run()
+    {
+        echo $this->prompt;
+        while ($__code__ = fgets($this->input)) {
+            try {
+                $this->_print($this->_eval($__code__));
+            } catch (Exception $e) {
+                echo $e . "\n";
+            }
+            echo $this->prompt;
+        }
+    }
+
+    /**
+     * Evaluate code
+     *
+     * @param string $_repl_code The code to eval
+     *
+     * @return mixed The output
+     */
+    private function _eval($_repl_code)
+    {
+        $_repl_code = trim($_repl_code);
+
+        // Add a trailing semicolon
+        if (substr($_repl_code, -1) != ';') {
+            $_repl_code .= ';';
+        }
+
+        // Make sure we get a value back from eval()
+        if (strpos($_repl_code, 'return') !== 0 &&
+            strpos($_repl_code, 'throw') !== 0) {
+            $_repl_code = 'return ' . $_repl_code;
+        }
+
+        ob_start(array($this, 'ob_cleanup'));
+        $out = null;
+        try {
+            $out = eval($_repl_code);
+        } catch (Exception $e) {
+            // Clean up
+            ob_flush();
+            ob_end_clean();
+            throw $e;
+        }
+        ob_flush();
+        ob_end_clean();
+        return $out;
+    }
+
+    /**
+     * Clean up output captured from eval()'d code
+     *
+     * @param string $output Output from the code
+     *
+     * @return string Cleaned up output
+     */
+    public function ob_cleanup($output)
+    {
+        if (strlen($output) > 0 && substr($output, -1) != "\n") {
+            $output .= "\n";
+        }
+        return $output;
+    }
+
+    /**
+     * Print the output of some code
+     *
+     * @param mixed $out The output
+     *
+     * @return void
+     */
+    private function _print($out)
+    {
+        $type = gettype($out);
+        switch ($type) {
+        case 'string':
+            printf("\"%s\"\n", $out);
+            break;
+        case 'integer':
+        case 'boolean':
+            var_dump($out);
+        break;
+
+        case 'array':
+            var_export($out);
+            echo "\n";
+            break;
+
+        default:
+            print_r($out);
+        }
+    }
+}
+
+if (basename($_SERVER['argv'][0]) == basename(__FILE__)) {
+    $_repl = new PHP_Repl;
+    $_repl->run();
+}
 
 ?>
